c4c2ef990d97751ec31f65551ca691285ecf3ec9
k-kuol <k.kuol@alustudent.com>
2026-02-26 11:21:05 +0200

    content

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for name in names:
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
