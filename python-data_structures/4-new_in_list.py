e1fec5b6761c2dac371de185556d038ad1d6c7c5
k-kuol <k.kuol@alustudent.com>
2026-03-10 17:11:40 +0200

    content

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,4 +4,4 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(copy):
         return copy
     copy[idx] = element
-    return copy 
+    return copy
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,4 +6,4 @@ def max_integer(my_list=[]):
     for i in my_list[1:]:
         if i > maximum:
             maximum = i
-    return maximum 
+    return maximum
