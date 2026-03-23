93c273f20ec889b99371fb583f8aeccf53790a30
k-kuol <k.kuol@alustudent.com>
2026-03-23 12:58:22 +0200

    Fix PEP8 issues in 12-roman_to_int.py: add docstrings and remove trailing whitespace

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+"""Roman to Integer conversion module"""
+
+
 def roman_to_int(roman_string):
+    """Convert a Roman numeral to an integer.
+    
+    Args:
+        roman_string: A string representing a Roman numeral
+        
+    Returns:
+        An integer value of the Roman numeral, or 0 if invalid
+    """
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_dict = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
+
     total = 0
     prev_value = 0
-    
+
     for char in reversed(roman_string):
         if char not in roman_dict:
             return 0
@@ -20,5 +31,5 @@ def roman_to_int(roman_string):
         else:
             total += value
         prev_value = value
-    
+
     return total
