7d3cddb5c082f0d513becb982502b62166cff32d
k-kuol <k.kuol@alustudent.com>
2026-04-03 15:37:51 +0200

    Fix tasks 1 and 4 in python-network_0

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the body of the response only for a 200 status code
-curl -s -w "%{http_code}" "$1" | awk '{a[NR]=$0} END{if(a[NR]=="200") for(i=1;i<NR;i++) print a[i]}'
+curl -s -f "$1"
